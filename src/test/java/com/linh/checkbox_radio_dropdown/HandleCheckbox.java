c2df407dd0c78124ff379be8207f7d5a6d511637
mylinh123-tech <mylinhtayvinh@gmail.com>
2026-05-09 14:36:23 +0700

    add package : checkbox- radio- dropdown

---
+++ src/test/java/com/linh/checkbox_radio_dropdown/HandleCheckbox.java
@@ -0,0 +1,21 @@
+package com.linh.checkbox_radio_dropdown;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class HandleCheckbox {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://crm.anhtester.com/admin");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("admin@example.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123456");
+    }
+}
